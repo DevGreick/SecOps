8e654fde361a1dea6423b8b141aec5a7a42ffda2
Jackson <jackson.greick@gmail.com>
2022-10-27 00:00:07 -0300

    modified:   ScriptScanDiretorio.py

--- ScriptScanDiretorio.py
+++ ScriptScanDiretorio.py
@@ -1,9 +1,7 @@
 import requests
-alvo = "http://pudim.com.br/"
+alvo = "https://www.terra.com.br/"
 lista_tentativas = ["",".git", "login", "painel", "mail"]
 for tentativa in lista_tentativas:
  r = requests.get (alvo+tentativa)
 if r.status_code ==200:
   print("encontrei ->", alvo+tentativa)
-else:
-  print("offline ->", alvo+tentativa)
