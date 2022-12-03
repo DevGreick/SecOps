53dc5eccb04067d8f15064d068fb6b5c51df9261
Jackson <jackson.greick@gmail.com>
2022-12-03 15:26:15 -0300

    Scanaumentado

--- ScriptScanDiretorio.py
+++ ScriptScanDiretorio.py
@@ -1,7 +1,12 @@
 import requests
 alvo = "https://www.terra.com.br/"
 lista_tentativas = ["",".git", "login", "painel", "mail"]
+lista_tentativas2 = ["admin", "frameworks", "internal", "errors", "noticias"]
 for tentativa in lista_tentativas:
  r = requests.get (alvo+tentativa)
-if r.status_code ==200:
-  print("encontrei ->", alvo+tentativa)
+ if r.status_code ==200:
+  print( alvo+tentativa)
+  for tentativa in lista_tentativas2:
+    r = requests.get (alvo+tentativa)
+ if r.status_code ==200:
+  print( alvo+tentativa)
