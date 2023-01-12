5732369d614dca42ec27e375066e09125ff0420f
Jackson <39761156+SecZeroR@users.noreply.github.com>
2023-01-12 01:25:28 -0300

    Update Scanvul.sh

--- Scanvul.sh
+++ Scanvul.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Script para buscar vulnerabilidades no site www.terra.com.br
+# Script para buscar vulnerabilidades no site www.exemplo.com.br
 
 echo "Iniciando a busca por vulnerabilidades no site www.exemplo.com.br..."
 
 # Utilizando o comando nmap para verificar as portas abertas do site: 
-nmap -sV www.terra.com.br 
+nmap -sV www.exemplo.com.br
 
 # Verificando se existem algumas vulnerabilidades conhecidas no site: 
-nikto -h www.terra.com.br 
+nikto -h www.exemplo.com.br
 
 # Utilizando o comando dirb para buscar arquivos e diretórios escondidos: 
-dirb http://www.terra.com.br/ 
+dirb http://www.exemplo.com.br/
 
  # Utilizando o comando sqlmap para verificar se existem falhas de segurança relacionadas ao banco de dados: 
-sqlmap -u http://www.terra.com.br/ --dbs
+sqlmap -u http://www.exemplo.com.br/ --dbs
 
  # Verificando se existem algumas vulnerabilidades desconhecidas no site: 
-wpscan --url http://www.terra.com.br/ --enumerate vp
+wpscan --url http://www.exemplo.com.br/ --enumerate vp
 
  # Verificando se existem falhas de segurança relacionadas ao servidor web do site: 
-uniscan -u http://www.terra.com.br/ -qweds
+uniscan -u http://www.exemplo.com.br/ -qweds
 
  # Verificação de qualquer outras possíveis vulnerabilidades no site:  
-nsec3map -d www terra com br
+nsec3map -d www exemplo com br
 
  # Caso não encontre nada, exibir uma mensagem informativa na tela:  
 if [ $? == 0 ]; then echo "Nenhuma vulnerabilidade foi encontrada."; else echo "Vulnerabilidades foram detectadas."; fi
