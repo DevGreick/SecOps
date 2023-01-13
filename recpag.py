e537fb97a1c1ca2db19df75874d425863080fa59
Jackson <jackson.greick@gmail.com>
2023-01-13 01:11:02 -0300

    modified:   recpag.py

--- recpag.py
+++ recpag.py
@@ -6,3 +6,6 @@ while True:  # Cria um loop infinito
     webbrowser.open('www.terra.com.br')  # Abre o site www.terra.com.br
 
     time.sleep(20)  # Aguarda 20 segundos antes de recarregar a página
+
+
+    
