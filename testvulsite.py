0c8ed2ba923546aae123513bdb3fb96457493cc8
Jackson <jackson.greick@gmail.com>
2023-01-13 14:08:57 -0300

    modified:   testvulsite.py

--- testvulsite.py
+++ testvulsite.py
@@ -17,30 +17,30 @@ def check_vulnerabilities(url: str) -> None:
         check_sql_injection(response.text)
         check_file_inclusion(response.text)
         
-        print("Website is secure.")
+        print("Website e seguro.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"um erro ocorreu: {e}")
 
 def check_xss(text: str) -> None:
     """
             # Busca vulnerabilidades cross-site scripting (XSS) .
     """
     if "<script>" in text or "<script" in text:
-        raise ValueError("XSS vulnerability detected.")
+        raise ValueError(" vulnerabilidade XSS detectado.")
 
 def check_sql_injection(text: str) -> None:
     """
      # Busca por SQL injection.
     """
     if "SELECT" in text or "UPDATE" in text or "DELETE" in text:
-        raise ValueError("SQL injection vulnerability detected.")
+        raise ValueError("SQL injection  detectado.")
 
 def check_file_inclusion(text: str) -> None:
     """
     # Busca  por arquivo vulneravel.
     """
     if "include(" in text or "require(" in text:
-        raise ValueError("File inclusion vulnerability detected.")
+        raise ValueError("arquivo vulneravel detectado.")
 
 # Exemplo de uso
 check_vulnerabilities("http://www.google.com")
