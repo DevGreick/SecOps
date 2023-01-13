26d184067d8ef700a45de22fc0e48691855a0b40
Jackson <jackson.greick@gmail.com>
2023-01-13 00:30:07 -0300

    deleted:    recarregarpagina.py new file:   recpag.py

---
+++ recpag.py
@@ -0,0 +1,8 @@
+import webbrowser  # Importa a biblioteca webbrowser
+import time  # Importa a biblioteca time
+
+while True:  # Cria um loop infinito
+
+    webbrowser.open('www.terra.com.br')  # Abre o site www.terra.com.br
+
+    time.sleep(20)  # Aguarda 20 segundos antes de recarregar a página
