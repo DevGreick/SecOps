ff5fc2cf81d5e1cc1b25a44667ad230df4dd9112
Jackson <jackson.greick@gmail.com>
2023-01-13 14:02:54 -0300

    modified:   testvulsite2.py

--- testvulsite2.py
+++ testvulsite2.py
@@ -1,41 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-def check_vulnerabilities(url: str) -> None:
+def check_vulnerabilidade(url: str) -> None:
     """
-    Check for vulnerabilities in a website.
+    Verifica vulnerabilidades no website.
     """
     try:
-        # Send a GET request to the website.
+        # Envia um GET request para o website.
         response = requests.get(url)
 
-        # Check the response status code.
+        # Checa se a resposta e 200.
         if response.status_code != 200:
             raise ValueError(f"Invalid status code: {response.status_code}")
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Check for cross-site scripting (XSS) vulnerabilities.
+        # Busca vulnerabilidades cross-site scripting (XSS) .
         for tag in soup.find_all(['script', 'a']):
             if 'javascript:' in tag.get('href', '') or 'javascript:' in tag.get('onclick', ''):
-                print(f"XSS vulnerability found in: {tag}")
+                print(f"XSS encontrado em: {tag}")
 
-        # Check for SQL injection vulnerabilities.
+        # Busca por SQL injection.
         for form in soup.find_all('form'):
             if 'sql' in form.get('action', ''):
-                print(f"SQL injection vulnerability found in: {form}")
+                print(f"SQL injection  encontrado em: {form}")
             for input_ in form.find_all('input'):
                 if 'sql' in input_.get('name', ''):
-                    print(f"SQL injection vulnerability found in: {input_}")
+                    print(f"vuln SQL injection  encontrado em: {input_}")
 
-        # Check for file inclusion vulnerabilities.
+        # Busca  por arquivo vulneravel.
         for tag in soup.find_all(['img', 'link']):
             if 'include' in tag.get('src', '') or 'require' in tag.get('src', ''):
-                print(f"File inclusion vulnerability found in: {tag}")
+                print(f"Arquivo  vulneravel encontrado em: {tag}")
 
-        print("Website is secure.")
+        print("Site seguro.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Um erro ocorreu: {e}")
 
-# Example usage
-check_vulnerabilities("http://www.terra.com.br")
+# Exemplo
+check_vulnerabilidade("http://www.terra.com.br")
